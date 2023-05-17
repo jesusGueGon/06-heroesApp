4d1328693dd732408d3c31bce490f41d84c6924e
jesusGueGon <jesus.guerrero@bosonit.com>
2023-05-17 18:26:00 +0200

    Seccion 16 Terminada Guards

---
+++ src/app/auth/services/auth.service.ts
@@ -0,0 +1,55 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environments } from 'src/environments/environments';
+import { User } from '../interfaces/user.interface';
+import { Observable, catchError, map, of, tap } from 'rxjs';
+import { Idd } from '../../../../../04-countryApp/src/app/countries/interfaces/country';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private baseUrl = environments.baseUrl;
+  private user?: User;
+
+  constructor(private http: HttpClient) { }
+
+  get currentUser(): User|undefined {
+    if(!this.user) return undefined;
+    return structuredClone(this.user);
+  }
+
+  login(email: string, password: string): Observable<User> {
+
+    return this.http.get<User>(`${this.baseUrl}/users/1`)
+      .pipe(
+        tap(resp => this.user = resp),
+        tap(resp => localStorage.setItem('token', 'lkgasfgagASG.saDGASgsd.sagd556'))
+      );
+  }
+
+  checkAuthentication(): Observable<boolean>{
+
+    if( !localStorage.getItem('token') ) return of(false);
+
+    const token = localStorage.getItem('token');
+
+    return this.http.get<User>(`${this.baseUrl}/users/1`)
+      .pipe(
+        tap( user => this.user = user ),
+        map( user => !!user ),
+        catchError( err => of(false) )
+      )
+
+  }
+
+  logout() {
+
+    this.user = undefined;
+    localStorage.clear();
+
+  }
+
+
+}
